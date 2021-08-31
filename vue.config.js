19ae106d46b57a7f57aa2a6da7daa6b5f20ba27d
UrBetsy <lifankeke@foxmail.com>
2021-08-31 18:22:58 +0800

    feat:部署

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,8 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ? '/lifankeke-learn/' : '/',
     parallel: require('os').cpus().length > 1,
     devServer: {
         port: 8083,
-        // proxy: {
-        //     '/rest': {
-        //         target: 'https://app-e-test1.corp.kuaishou.com/',
-        //         changeOrigin: true,
-        //     },
-        // },
         disableHostCheck: true,
     },
     transpileDependencies: ['vuex-composition-helpers', 'vux'],
